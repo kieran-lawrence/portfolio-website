a89e2f5589bf308ea081d10f26f340845a9139c9
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-04-24 17:32:18 +0800

    Refactor header & theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,21 +3,29 @@ import ThemeToggle from './themeToggle';
 
 export default function SiteHeader() {
 	return (
-		<header>
-			<ThemeToggle />
-			<nav>
-				<ul className="flex gap-4">
-					<li>
-						<Link href="/">Home</Link>
-					</li>
-					<li>
-						<Link href="#resume">Resume</Link>
-					</li>
-					<li>
-						<Link href="#portfolio">Portfolio</Link>
-					</li>
-				</ul>
-			</nav>
+		<header className="sticky top-8 z-10 m-8 flex items-center justify-center">
+			<div className="animatedHeader flex w-[80vw] justify-between rounded-2xl px-8 py-4">
+				<nav>
+					<ul className="flex gap-8 text-xl">
+						<li className="headNavItem">
+							<Link href="/" className="outline-0">
+								Home
+							</Link>
+						</li>
+						<li className="headNavItem">
+							<Link href="#resume" className="outline-0">
+								Resume
+							</Link>
+						</li>
+						<li className="headNavItem">
+							<Link href="#portfolio" className="outline-0">
+								Portfolio
+							</Link>
+						</li>
+					</ul>
+				</nav>
+				<ThemeToggle />
+			</div>
 		</header>
 	);
 }
