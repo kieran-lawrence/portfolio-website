88efc3ff032b78be0c3dbfa125887abfe8e1f4a6
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-05-09 18:09:08 +0800

    Fix theme defaults

--- src/components/themeToggle.tsx
+++ src/components/themeToggle.tsx
@@ -1,16 +1,20 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import IconSun from './icons/iconSun';
 import IconMoon from './icons/iconMoon';
 
 export default function ThemeToggle() {
+	const [isDark, setIsDark] = useState(false);
 	// Set the initial theme based on the user's system preference
 	useEffect(() => {
 		if (document && typeof window !== 'undefined') {
 			const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 			if (systemTheme) {
 				document.body.dataset.theme = 'dark';
+				setIsDark(true);
+			} else {
+				setIsDark(false);
 			}
 		}
 	}, []);
@@ -18,13 +22,19 @@ export default function ThemeToggle() {
 	const handleToggleClick = () => {
 		if (document) {
 			const { dataset } = document.body;
-			dataset.theme = dataset.theme === 'dark' ? 'light' : 'dark';
+			dataset.theme = isDark ? 'light' : 'dark';
+			setIsDark((prev) => !prev);
 		}
 	};
 
 	return (
 		<label className="relative inline-block h-9 w-[4.5rem]" aria-label="Toggle Theme">
-			<input type="checkbox" className="themeToggleInput" onChange={handleToggleClick} />
+			<input
+				type="checkbox"
+				className="themeToggleInput"
+				onChange={handleToggleClick}
+				checked={!isDark}
+			/>
 			<span className="themeToggleSlider">
 				<IconSun />
 				<IconMoon />
