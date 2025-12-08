cfbf484c94da0c9e197336e8aa971a2c0058aa27
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2025-12-08 10:50:15 +0800

    last tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,9 +28,9 @@ export default function Home() {
 			/>
 			<div
 				id="intro"
-				className="mt-32 flex w-screen flex-col items-center justify-center gap-4 md:mt-40 md:gap-8"
+				className="flex w-screen flex-col items-center justify-center gap-4 pt-32 md:mt-40 md:gap-8"
 			>
-				<h1 className="pt-24 text-6xl md:text-8xl">Hi, I&apos;m Kieran.</h1>
+				<h1 className="text-6xl md:text-8xl">Hi, I&apos;m Kieran.</h1>
 				<h3 className="w-3/4 text-center text-lg md:w-1/2 md:text-xl 2xl:w-1/4">
 					an Australian-based software developer driven by a passion for creating cool and
 					meaningful technology
